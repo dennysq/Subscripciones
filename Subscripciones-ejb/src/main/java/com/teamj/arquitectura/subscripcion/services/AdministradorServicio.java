3c2657890ef3b1ce6d4d93948c435748223c310f
Klever <Klever@Klever-PC>
2016-08-12 13:55:12 -0500

    mapeo completo.. probar servicios: administrador, usuario

---
+++ Subscripciones-ejb/src/main/java/com/teamj/arquitectura/subscripcion/services/AdministradorServicio.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamj.arquitectura.subscripcion.services;
+
+import com.teamj.arquitectura.subscripcion.dao.AdministradorDAO;
+import com.teamj.arquitectura.subscripcion.model.Administrador;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.validation.ValidationException;
+
+/**
+ *
+ * @author Klever
+ */
+@Stateless
+@LocalBean
+public class AdministradorServicio implements Serializable{
+    @EJB
+    private AdministradorDAO administradorDAO;
+    
+    public Administrador obtenerPorId(Integer id) {
+        return this.administradorDAO.findById(id, false);
+    }
+    public List<Administrador> retrieveAdministrador() {
+        return this.administradorDAO.findAll();
+    }
+    
+    public void registrarAdmi(Administrador a) throws ValidationException {
+        //boolean flag = false;
+        Administrador temp = new Administrador();
+        try {
+        temp.setEmail(a.getEmail());
+//        String codecPassword = DigestUtils.md5Hex(u.getPassword());
+//        temp.setPassword(codecPassword);
+
+        temp.setPassword(a.getPassword());
+        temp.setEstado(a.getEstado());
+
+        administradorDAO.insert(temp);
+        } catch (Exception e) {
+            throw new ValidationException("Error al crear un nuevo usuario", e);
+        }
+    }
+    
+    public boolean editarAdmi(Administrador a) throws ValidationException {
+        boolean flag = false;
+//        String codecPassword = DigestUtils.md5Hex(u.getPassword());
+//        u.setPassword(codecPassword);
+        try {
+            this.administradorDAO.update(a);
+            flag = true;
+        } catch (Exception e) {
+            throw new ValidationException("Error al editar al administrador", e);
+        }
+        return flag;
+    }
+    
+    public void eliminarAdmi(Integer id) {
+        Administrador temp = this.administradorDAO.findById(id, false);
+        if (temp != null) {
+            this.administradorDAO.remove(temp);
+        }
+    }
+    
+    
+//    public Usuario retrieveUserLoged(Usuario u){
+//        Usuario tempUsu = new Usuario();
+//        tempUsu.setUsername(u.getUsername());
+//
+//        List<Usuario> tempList = this.usuarioDAO.find(tempUsu);
+//        if (tempList != null && tempList.size() == 1) {
+//            if (DigestUtils.md5Hex(u.getPassword()).equals(tempList.get(0).getPassword())) {
+//                return tempList.get(0);
+//            }
+//        }
+//        return null;
+//    }
+}
---
+++ Subscripciones-ejb/src/main/java/com/teamj/arquitectura/subscripcion/services/UsuarioServicio.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.teamj.arquitectura.subscripcion.services;
+
+import com.teamj.arquitectura.subscripcion.dao.UsuarioDAO;
+import com.teamj.arquitectura.subscripcion.model.Usuario;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.validation.ValidationException;
+
+/**
+ *
+ * @author Klever
+ */
+@Stateless
+@LocalBean
+public class UsuarioServicio implements Serializable{
+    @EJB
+    private UsuarioDAO usuarioDAO;
+    
+    public Usuario obtenerPorId(Integer id) {
+        return this.usuarioDAO.findById(id, false);
+    }
+    public List<Usuario> retrieveUsers() {
+        return this.usuarioDAO.findAll();
+    }
+    
+    public void registrarUsu(Usuario u) throws ValidationException {
+        //boolean flag = false;
+        Usuario temp = new Usuario();
+        try {
+        temp.setEmail(u.getEmail());
+//        String codecPassword = DigestUtils.md5Hex(u.getPassword());
+//        temp.setPassword(codecPassword);
+
+        temp.setNombres(u.getNombres());
+        temp.setApellidos(u.getApellidos());
+        temp.setGenero(u.getGenero());
+        usuarioDAO.insert(temp);
+        } catch (Exception e) {
+            throw new ValidationException("Error al crear un nuevo usuario", e);
+        }
+    }
+    
+    public boolean editarUsu(Usuario u) throws ValidationException {
+        boolean flag = false;
+//        String codecPassword = DigestUtils.md5Hex(u.getPassword());
+//        u.setPassword(codecPassword);
+        try {
+            this.usuarioDAO.update(u);
+            flag = true;
+        } catch (Exception e) {
+            throw new ValidationException("Error al editar al usuario", e);
+        }
+        return flag;
+    }
+    
+    public void eliminarUsu(Integer id) {
+        Usuario temp = this.usuarioDAO.findById(id, false);
+        if (temp != null) {
+            this.usuarioDAO.remove(temp);
+        }
+    }
+    
+    public Usuario retrieveUserLoged(String usuario, String password){
+        Usuario tempUsu = new Usuario();
+        tempUsu.setEmail(usuario);
+
+        List<Usuario> tempList = this.usuarioDAO.find(tempUsu);
+        if (tempList != null && tempList.size() == 1) {
+            if (password.equals(tempList.get(0).getEmail())) {
+                return tempList.get(0);
+            }
+        }
+        return null;
+    }
+    
+//    public Usuario retrieveUserLoged(Usuario u){
+//        Usuario tempUsu = new Usuario();
+//        tempUsu.setUsername(u.getUsername());
+//
+//        List<Usuario> tempList = this.usuarioDAO.find(tempUsu);
+//        if (tempList != null && tempList.size() == 1) {
+//            if (DigestUtils.md5Hex(u.getPassword()).equals(tempList.get(0).getPassword())) {
+//                return tempList.get(0);
+//            }
+//        }
+//        return null;
+//    }
+}
